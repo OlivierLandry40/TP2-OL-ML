21d5b3e123512c329e78c20e92275c2c3194de81
mathisLanglois54 <178506608+MathisLanglois36@users.noreply.github.com>
2026-03-23 12:14:37 -0400

    creer le setCode pour pour avoir comme condition avoir 3 majuscules

---
+++ TP2-OL/src/livres/Pays.java
@@ -0,0 +1,33 @@
+package livres;
+
+
+public class Pays {
+    private String nom;
+    private String code;
+
+    public Pays (String nom, String code){
+        this.nom = nom;
+        setCode(code);
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        if (code != null && code.matches("[A-Z]{3}"))
+            this.code = code;
+        else{
+            System.out.println("Erreur il doit y avoir exacteemnt 3 majuscules");
+        }
+    }
+
+}
